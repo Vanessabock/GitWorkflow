5950d73a27572c10786a0f12191813d9c7e7d588
vanessabock <vanessabock54@gmail.com>
2023-12-14 11:38:07 +0100

    Add coordinateList

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -1,9 +1,16 @@
 package de.neuefische;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-
+        List<Coordinates> coordinates = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            coordinates.add(new Coordinates(i, i));
+        }
+        System.out.println(coordinates);
     }
 }
